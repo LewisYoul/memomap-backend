863c79bc3a24fe9fdd5cbe36083d8a7cab92348a
Lewis <lewisyoul@gmail.com>
2018-09-01 17:01:40 +0100

    [feature] add basic index route for journeys

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :markers
 
+  resources :journeys
+
   namespace :api do
     resources :markers
   end
