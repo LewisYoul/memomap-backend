4b78072807fa705bda0e9e5682bc8e6d575d1bae
Lewis <lewisyoul@gmail.com>
2018-09-01 17:49:36 +0100

    [feature] create show controller action for Journeys

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,6 +1,18 @@
 class JourneysController < ApplicationController
 
+  before_action :set_journey, only: %i[show edit update destroy]
+
   def index
     @journeys = Journey.all
+  end
+
+  def show
+    @markers = @journey.markers
+  end
+
+  private
+
+  def set_journey
+    @journey = Journey.find(params[:id])
   end
 end
